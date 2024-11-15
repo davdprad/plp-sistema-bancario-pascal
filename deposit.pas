a16db5518d42b3a97bceb033120e6d627844771e
Bruna Lucas <jsantos.bu26@gmail.com>
2024-11-15 13:45:41 -0400

    Deletar o arquivo errado

--- deposit.pas
+++
@@ -1,8 +0,0 @@
-procedure Depositar(var conta: TConta; valor: real);
-begin
-    conta.saldo := conta.saldo + valor;
-    conta.transacoes := conta.transacoes + 1;
-    conta.historico[conta.transacoes] := 'Depósito: ' + FloatToStr(valor);
-    writeln('Depósito realizado com sucesso!');
-end;
-
