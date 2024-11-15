c4681e581a5b4d51c50daec27e7b49954d365479
Lucas Eduardo <lucaseduumoura@gmail.com>
2024-11-14 20:05:11 -0400

    feat: consertando pequenos erros

--- main.pas
+++ main.pas
@@ -1,5 +1,17 @@
 program HelloWorld;
 
+type
+    TConta = record
+        id: integer;
+        saldo: real;
+        historico: array[1..100] of string;
+        transacoes: integer;
+    end;
+
+var
+    contas: array[1..100] of TConta;
+    numContas: integer;
+
 begin
     writeln('Hello, World!');
 end.
